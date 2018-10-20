3c737d009064e30ee7902bf786be459130678bbd
Igar Ramaddhan <igarramaddhan@gmail.com>
2018-10-20 22:16:54 +0700

    add chart js

--- pages/home.js
+++ pages/home.js
@@ -7,6 +7,7 @@ import { getCandidates, vote } from '../libs/api';
 import styled from 'styled-components';
 import { colors } from '../libs/metrics';
 import Container from '../components/Container';
+import RC2 from 'react-chartjs2';
 
 type Props = {
   uid: string
@@ -47,6 +48,32 @@ class Home extends Component<Props, State> {
               overflowY: 'scroll'
             }}
           >
+            {/* <RC2
+              data={{
+                labels: [
+                  'January',
+                  'February',
+                  'March',
+                  'April',
+                  'May',
+                  'June',
+                  'July'
+                ],
+                datasets: [
+                  {
+                    label: 'My First dataset',
+                    backgroundColor: 'rgba(255,99,132,0.2)',
+                    borderColor: 'rgba(255,99,132,1)',
+                    borderWidth: 1,
+                    hoverBackgroundColor: 'rgba(255,99,132,0.4)',
+                    hoverBorderColor: 'rgba(255,99,132,1)',
+                    data: [65, 59, 80, 81, 56, 55, 40]
+                  }
+                ]
+              }}
+              // options={chartOptions}
+              type="bar"
+            /> */}
             {this.props.candidates.map(val => (
               <Card
                 disabled={this.props.user.voted}
